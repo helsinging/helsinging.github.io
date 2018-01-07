3ff8682fc146ebdf1d36aa77ebc8b1f1e0a6faad
Han Xiao <han.xiao@aalto.fi>
2018-01-07 12:35:59 +0000

    using reveal.js

--- assets/js/hxc.js
+++ assets/js/hxc.js
@@ -1,7 +1,43 @@
 $(document).ready(function(){
-    $('.grid').masonry({
-	itemSelector: '.grid-item',
-	columnWidth: 200
-    });
+    // console.log('document is ready');
 
+    Reveal.initialize({
+       	width: "80%",
+        height: "80%"
+    });
+    
+    // $('.grid').each(function(){
+    // 	$(this).masonry({
+    // 	    itemSelector: '.grid-item',
+    // 	    columnWidth: 200
+    // 	});
+    // });
+    // var $grid = $('.grid').masonry({
+    // 	itemSelector: '.grid-item',
+    // 	columnWidth: 200
+    // });
+
+    // $grid.on('layoutComplete', function(){
+    // 	console.log('layout done');
+    // 	// not called
+    // });
+
+    // Reveal.addEventListener( 'ready', function( event ) {
+	
+    // });
+
+
+    // More info about config & dependencies:
+    // - https://github.com/hakimel/reveal.js#configuration
+    // - https://github.com/hakimel/reveal.js#dependencies
+    // Reveal.initialize({
+    //    	width: "80%",
+    //     height: "80%",
+    // 	dependencies: [
+    // 	    { src: '/assets/reveal.js/plugin/markdown/marked.js' },
+    // 	    { src: '/assets/reveal.js/plugin/markdown/markdown.js' },
+    // 	    { src: '/assets/reveal.js/plugin/notes/notes.js', async: true },
+    // 	    { src: '/assets/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    // 	]
+    // });    
 })  
